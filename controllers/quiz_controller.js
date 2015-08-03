5848561e8afc3e87bf1502c3d4f0457bce05553c
Oscar Rodríguez <balmon4all@gmail.com>
2015-08-03 17:22:01 +0200

    Validación de entradas
    Se incluye el control para validar la entrada de cadenas vacías en la
    creación de preguntas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -25,13 +25,13 @@ exports.index = function(req, res) {
 			srch = "%"+srch.replace(/ /g,"%")+"%";
 		}
 		models.Quiz.findAll({where:["LOWER(pregunta) like LOWER(?)", srch]}).then(function(quizes){
-			res.render('quizes/index',{ quizes: quizes});
+			res.render('quizes/index',{ quizes: quizes, errors: []});
 		}).catch(function(error) { next(error); });
 };
 
 // GET /quizes/:quizId(\\d+)
 exports.show = function (req, res) {
-		res.render('quizes/show', {quiz: req.quiz});
+		res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:quizId(\\d+)/answer
@@ -41,7 +41,7 @@ exports.answer = function (req, res) {
 			resultado = 'Correcto';
 		}
 			res.render('quizes/answer',
-									{quiz: req.quiz, respuesta: resultado});
+									{quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/new
@@ -49,15 +49,25 @@ exports.new = function(req, res) {
 	var quiz = models.Quiz.build(
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // GET /quizes/create
 exports.create = function(req, res){
 	var quiz = models.Quiz.build( req.body.quiz);
 
-	//Guardar en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	})
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+				//Guardar en DB los campos pregunta y respuesta de quiz
+				quiz
+					.save({fields: ["pregunta", "respuesta"]})
+					.then(	function(){ res.redirect('/quizes'); });
+			}
+		}
+	);
 };
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 // Autoload de comandos con :quizId
@@ -17,8 +17,9 @@ router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
 router.get('/quizes/new',                   quizController.new);
 router.post('/quizes/create',               quizController.create);
-router.get('/author',function(req,res) {
-  	res.render('author');
+router.get('/author',
+function(req,res) {
+  	res.render('author', {errors: []} );
  });
 
 module.exports = router;
